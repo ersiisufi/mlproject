e30d2be3b1c70bde6e7e39bdbfaf37b24c417717
Ersi Isufi <ersi.isufi.15@gmail.com>
2026-01-02 00:15:23 +0300

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 
-def error_message_details(error, error_detail:sys):
+def error_message_details(error, error_detail):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
@@ -9,7 +9,7 @@ def error_message_details(error, error_detail:sys):
     return error_message        
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail)
 

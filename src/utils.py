59612884e5c7f398a88e9cc5f5555cd6eb2d14f6
Ersi Isufi <ersi.isufi.15@gmail.com>
2026-01-02 03:03:06 +0300

    Hyperparamether training

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,20 +21,26 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, X_test, y_train, y_test, models):
+def evaluate_model(X_train, X_test, y_train, y_test, models, params ):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
             model_name = list(models.keys())[i]
+            para=params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            # model.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            y_traun_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            y_train_pred = gs.predict(X_train)
+            y_test_pred = gs.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_traun_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
